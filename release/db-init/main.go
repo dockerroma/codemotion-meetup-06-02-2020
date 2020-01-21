0c522da95b10d549e4841395cb7447cf007fb833
micro <matteo.damico@konicaminolta.it>
2020-01-21 18:20:43 +0100

    Moved db-init in released Modified docker-compose to add db-init Added env variable to db-init ( main.go ) in order to configure it directly from docker-compose file

---
+++ release/db-init/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+    "context"
+    "fmt"
+    "log"
+    "os"
+    "time"
+    "bufio"
+//  "go.mongodb.org/mongo-driver/bson"
+    "go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Guest struct {
+    name string
+    
+}
+
+var (
+        connectionUrl = "mongodb://"+os.Getenv("DB_USERNAME")+":"+os.Getenv("DB_PASSWORD")+"@"+os.Getenv("DB_HOST")+":"+os.Getenv("DB_PORT")
+	database = "test"
+	collection = "guest"
+	insertDataStatementsFilePath ="insert.sql";
+)
+
+func getConnection()(*mongo.Client, error) {
+        // Set client options
+	clientOptions := options.Client().ApplyURI(connectionUrl)
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+       		log.Fatal(err)
+	}
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+    		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+	return client,err;
+
+}
+
+func insertRows(collection *mongo.Collection,err error,filepath string){
+    file, err := os.Open(filepath)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        fmt.Println(scanner.Text())
+	oneguest := Guest{name: scanner.Text()}
+        insertResult, err := collection.InsertOne(context.TODO(), oneguest)
+	if err != nil {
+    		log.Fatal(err)
+	}
+	fmt.Println("Inserted a single document: ", insertResult.InsertedID)
+    }
+    if erro := scanner.Err(); erro != nil {
+        log.Fatal(erro)
+    }
+}
+
+func main() {
+	client, err := getConnection()
+        ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+        err = client.Connect(ctx)
+
+	collection := client.Database(os.Getenv("DB_DATABASE")).Collection(os.Getenv("DB_COLLECTION"))
+
+	
+	insertRows(collection,err, insertDataStatementsFilePath);	
+	log.Print("Database initialization completed.")
+
+
+	err = client.Disconnect(context.TODO())
+	
+	if err != nil {
+	    log.Fatal(err)
+	}
+	fmt.Println("Connection to MongoDB closed.")
+}
